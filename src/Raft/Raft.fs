9bbb0f00b80ad0b5a26df43a1387e52fca155044
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2017-12-17 18:28:15 +0100

    stash/unstash semantics and single replication pipeline

--- src/Raft/Raft.fs
+++ src/Raft/Raft.fs
@@ -40,12 +40,23 @@ type Effect<'id,'msg> =
     | Schedule   of key:string * after:TimePart * receiver:'id * message:'msg
     /// Unschedule previously scheduled message (or ignore if it was not found).
     | Unschedule of key:string
+    | Stash      of message:'msg
+    | Unstash
     
 type Settings = 
     { ElectionTimeout: TimeSpan;
       HeartbeatTimeout: TimeSpan }         
     
-type Committable<'op> = { Commit:bool; CorrelationId:CorrelationId; Operation:'op }
+type Commit<'op> = CorrelationId * 'op
+
+[<Struct>]
+type Committable<'op> = 
+    | Committed of committed:Commit<'op>
+    | Uncommitted of uncommitted:Commit<'op>
+    member x.CorrelationId =
+        match x with
+        | Committed(id, _) -> id
+        | Uncommitted(id, _) -> id
 
 type ReplicationProgress<'id,'op when 'id : comparison> =
     { Message: Committable<'op>; ReplyTo: 'id; Remaining: Set<'id> }
@@ -57,21 +68,20 @@ type State<'id,'s,'op when 'id : comparison> =
       Self: 'id                 // Unique node identifier in scope of the current cluster.
       KnownNodes: Set<'id>      // List of nodes known to the present node.
       Epoch: int                // Info about the latest known election.
-      Uncommited: Map<CorrelationId, 'op> // Operations waiting to be comitted
       State: 's }               // Entries used for Raft log replication.
 
 /// Contains info about Raft node state with internal details. Some Raft roles
 /// may define some extra data necessary to perform their duties.
 type NodeState<'id,'s,'op when 'id : comparison> =
     /// Initial state of the node or follower with well-known Leader.
-    | Follower of state:State<'id,'s,'op> * leader:'id
+    | Follower of state:State<'id,'s,'op> * leader:'id * lastCommit:CorrelationId option
     /// Candidate for the new leader. Has it's vote counter.
     | Candidate of state:State<'id,'s,'op> * voters:Set<'id>
     /// Current leader of Raft cluster.
-    | Leader of state:State<'id,'s,'op> * replication:Map<CorrelationId, ReplicationProgress<'id,'op>>
+    | Leader of state:State<'id,'s,'op> * progress:ReplicationProgress<'id,'op> option
     member x.State =
         match x with
-        | Follower(state,_)  -> state
+        | Follower(state,_,_)  -> state
         | Candidate(state,_) -> state
         | Leader(state,_)    -> state
       
@@ -91,7 +101,7 @@ type Message<'id,'op when 'id : comparison> =
     | UpdateSuccess   of corrId:CorrelationId
     | UpdateFailure   of corrId:CorrelationId
     // Request send from the Leader to its Followers to replicate the state
-    | AppendEntries   of epoch:int * leader:'id * ops: Committable<'op> list
+    | AppendEntries   of epoch:int * leader:'id * op:Committable<'op> option
     // Response to Replicate request: follower confirms that replication succeeds.
     | Confirm         of corrId:CorrelationId * follower:'id
 
@@ -108,22 +118,19 @@ let inline scheduleHeartbeatTimeout state =
 
 let inline scheduleElectionTimeout state =
     Schedule(state.Self + "-eltimeout", After state.Settings.ElectionTimeout, state.Self, BecomeCandidate(state.Epoch+1))
+        
+let commit =
+    function
+    | Uncommitted(id, op) -> Committed(id, op)
+    | other -> other
 
-let apply update state committable =
-    if committable.Commit 
-    then 
-        let committed = update state.State committable.Operation
-        { state with State = committed; Uncommited = Map.remove committable.CorrelationId state.Uncommited }
-    else
-        { state with Uncommited = Map.add committable.CorrelationId committable.Operation state.Uncommited }
-    
 let rec raft update behavior message =
     match behavior, message with
 
     // Follower was nominated to candidate for the next epoch. It nominates itself
     // and request all known nodes for their votes. Also schedules timeout after which
     // it will retry election again (useful, when requests were lost).
-    | Follower(state, _), BecomeCandidate epoch when epoch > state.Epoch ->
+    | Follower(state, _, _), BecomeCandidate epoch when epoch > state.Epoch ->
         let requestVote = RequestVote(epoch, state.Self)
         let leaflets = 
             state.KnownNodes 
@@ -137,36 +144,53 @@ let rec raft update behavior message =
     // Follower was requested to vote on another candidate for the next epoch.
     // Older epoch has higher precedence, so no matter what follower will always to vote
     // on that candidate.
-    | Follower(state, _), RequestVote(epoch, candidate) when epoch > state.Epoch ->
+    | Follower(state, _, _), RequestVote(epoch, candidate) when epoch > state.Epoch ->
         let vote = Send(candidate, Vote(epoch, state.Self, candidate))
-        Become(raft update, Follower({ state with Epoch = state.Epoch }, candidate), [vote])
+        Become(raft update, Follower({ state with Epoch = state.Epoch }, candidate, None), [vote])
 
     // Follower was requested to vote on candidate in current epoch. However
     // this also means, that it has already given it's vote for another candidate.
     // So it responds with existing vote.
-    | Follower(state, leader), RequestVote(epoch, candidate) ->
+    | Follower(state, leader, _), RequestVote(epoch, candidate) ->
         let alreadyVoted = Send(candidate, Vote(epoch, state.Self, leader))
         Become(raft update, behavior, [alreadyVoted])
 
     // Follower got request to append entries, but it's not his responsibility.
     // It forwards this request to the leader instead
-    | Follower(_, leader), Update _ ->
+    | Follower(_, leader, _), Update _ ->
         let send = Send(leader, message)
         Become(raft update, behavior, [send])
-        
-    // Follower received replication request at higher or current epoch. It updates its state (uncommitted) and replies.
-    | Follower(state, _), AppendEntries(epoch, leader, ops) when epoch >= state.Epoch ->
+       
+    // Follower received heartbeat from the current or newer leader.
+    // Refresh timeout and optionally update leader.
+    | Follower(state, _, commit), AppendEntries(epoch, leader, None) when epoch >= state.Epoch ->
         let timeout = Schedule(state.Self+"-candidating", After state.Settings.HeartbeatTimeout, state.Self, BecomeCandidate(state.Epoch + 1))
-        let state' = ops |> List.fold (fun acc op -> apply update acc op) state
-        let confirmations = ops |> List.map (fun op -> Send(leader, Confirm(op.CorrelationId, state'.Self)))
-        Become(raft update, Follower(state', leader), timeout::confirmations)
+        Become(raft update, Follower(state, leader, commit), [timeout])
+        
+    // Follower received replication request at higher or current epoch. All previously 
+    // uncommitted changes are discarded.
+    | Follower(state, _, _), AppendEntries(epoch, leader, Some(Uncommitted(id, _))) when epoch >= state.Epoch ->
+        let timeout = Schedule(state.Self+"-candidating", After state.Settings.HeartbeatTimeout, state.Self, BecomeCandidate(state.Epoch + 1))
+        let confirm = Send(leader, Confirm(id, state.Self))
+        Become(raft update, Follower(state, leader, Some id), [timeout;confirm])
+        
+    // Follower received committed request. Update internal state.
+    | Follower(state, _, commit), AppendEntries(epoch, leader, Some(Committed(id, op))) when epoch >= state.Epoch->
+        let timeout = Schedule(state.Self+"-candidating", After state.Settings.HeartbeatTimeout, state.Self, BecomeCandidate(state.Epoch + 1))
+        let state' = 
+            match commit with
+            | Some id' when id' <> id -> behavior // there was a pending commit, but IDs don't match
+            | _ -> 
+                let next = update state.State op
+                Follower({state with State = next }, leader, None)
+        Become(raft update, state', [timeout])
         
     // Candidate was requested to vote on another candidate in higher epoch, which 
     // takes precedence over him. It becomes new follower and gives its vote on the
     // candidate.
     | Candidate(state, _), RequestVote(epoch, candidate) when epoch > state.Epoch ->
         let vote = Send(candidate, Vote(epoch, state.Self, candidate))
-        Become(raft update, Follower({ state with Epoch = epoch }, candidate), [vote])
+        Become(raft update, Follower({ state with Epoch = epoch }, candidate, None), [vote])
 
     // Candidate was requested to vote on another candidate the same of lover epoch.
     // It responds, that it already voted on himself.
@@ -184,62 +208,78 @@ let rec raft update behavior message =
             // candidate has reached majority of votes, it can become a leader
             let interval = Repeat(TimeSpan.Zero, state.Settings.HeartbeatTimeout)
             let heartbeat = Schedule(state.Self + "-heartbeat", interval, state.Self, HeartbeatTick)
-            Become(raft update, Leader(state, Map.empty), [heartbeat])
+            Become(raft update, Leader(state, None), [heartbeat])
         else
             // candidate is still waiting for remaining votes
             Become(raft update, Candidate(state, voters'), [])
 
     // Candidate received heartbeat from higher or equal epoch. It means it had loose
     // the election, so it becomes follower instead.
-    | Candidate(state, _), AppendEntries(epoch, leader, _) when epoch >= state.Epoch ->
+    | Candidate(state, _), AppendEntries(epoch, leader, None) when epoch >= state.Epoch ->
         let timeout = Schedule(state.Self+"-candidating", After state.Settings.HeartbeatTimeout, state.Self, BecomeCandidate(state.Epoch + 1))
-        Become(raft update, Follower({ state with Epoch = epoch }, leader), [timeout])
+        let state'= { state with Epoch = epoch }
+        Become(raft update, Follower(state', leader, None), [timeout])
+
+    // Candidate received heartbeat from higher or equal epoch. It means it had loose
+    // the election, so it becomes follower instead. Also confirms uncommited request.
+    | Candidate(state, _), AppendEntries(epoch, leader, Some(Uncommitted(id,_))) when epoch >= state.Epoch ->
+        let timeout = Schedule(state.Self+"-candidating", After state.Settings.HeartbeatTimeout, state.Self, BecomeCandidate(state.Epoch + 1))
+        let confirm = Send(leader, Confirm(id, state.Self))
+        let state'= { state with Epoch = epoch }
+        Become(raft update, Follower(state', leader, Some id), [timeout;confirm])
+
+    // Candidate received heartbeat from higher or equal epoch. It means it had loose
+    // the election, so it becomes follower instead. Also immediatelly applies an update.
+    | Candidate(state, _), AppendEntries(epoch, leader, Some(Committed(id,op))) when epoch >= state.Epoch ->
+        let timeout = Schedule(state.Self+"-candidating", After state.Settings.HeartbeatTimeout, state.Self, BecomeCandidate(state.Epoch + 1))
+        let confirm = Send(leader, Confirm(id, state.Self))
+        let updated = update state.State op
+        let state'= { state with Epoch = epoch; State = updated }
+        Become(raft update, Follower(state', leader, None), [timeout;confirm])
         
     // On a heartbeat tick, we'll send updated AppendEntries
-    | Leader(state, progresses), HeartbeatTick ->
-        let others = state.KnownNodes |> Set.remove state.Self
-        let msgs = 
-            progresses 
-            |> Map.toSeq
-            |> Seq.map (fun (_, p) -> p.Message)
-            |> Seq.toList
+    | Leader(state, progress), HeartbeatTick ->
+        let op = progress |> Option.map (fun p -> p.Message)
+        let msg = AppendEntries(state.Epoch, state.Self, op)
         let broadcast = 
-            others
-            |> Seq.map (fun node -> Send(node, AppendEntries(state.Epoch, state.Self, msgs)))
+            state.KnownNodes 
+            |> Set.remove state.Self
+            |> Seq.map (fun node -> Send(node, msg))
             |> Seq.toList        
         Become(raft update, behavior, broadcast)
 
+    // Leader received next update, but previous is being committed. Stash it.
+    | Leader(_, Some { Message = pending }), Update(corrId, _, _) ->
+        if pending.CorrelationId <> corrId
+        then Become(raft update, behavior, [Stash message])
+        else Become(raft update, behavior, []) // ignore redelivered request
+
     // Leader received new set entry request
-    | Leader(state, replications), Update(corrId, replyTo, op) ->
+    | Leader(state, None), Update(corrId, replyTo, op) ->
         let remaining = 
             state.KnownNodes
             |> Set.remove state.Self
-        let committable = { CorrelationId = corrId; Operation = op; Commit = false }
-        let entry = { ReplyTo = replyTo; Remaining = remaining; Message = committable }
-        let replications' = Map.add corrId entry replications
-        let state' = apply update state committable
-        Become(raft update, Leader(state', replications'), [])
+        let uncommitted = Uncommitted(corrId, op)
+        let entry = { ReplyTo = replyTo; Remaining = remaining; Message = uncommitted }
+        Become(raft update, Leader(state, Some entry), []) // no need to send anything, uncomitted will be picked during next heartbeat
     
     // Leader received confirmed replication response from follower
-    | Leader(state, replications), Confirm(id, follower) ->
-        let progress = Map.find id replications
+    | Leader(state, Some progress), Confirm(id, follower) when progress.Message.CorrelationId = id ->
         let remaining = Set.remove follower progress.Remaining
         if isMinority remaining state.KnownNodes
         then
             // replication reached majority of the nodes
-            let commit = { progress.Message with Commit = true }
             let reply = Send (progress.ReplyTo, UpdateSuccess id)
-            let append = AppendEntries(state.Epoch, state.Self, [commit])
+            let append = AppendEntries(state.Epoch, state.Self, Some (commit progress.Message))
             let commits = 
                 state.KnownNodes 
                 |> Set.remove state.Self
                 |> Seq.map (fun node -> Send(node, append))
                 |> Seq.toList
-            let replications' = Map.remove id replications
-            Become(raft update, Leader(state, replications'), reply::commits)
+            Become(raft update, Leader(state, None), reply::commits)
         else
             // there are still nodes waiting for replication
-            let replications' = Map.add id { progress with Remaining = remaining } replications
-            Become(raft update, Leader(state, replications'), [])
+            let progress' = { progress with Remaining = remaining }
+            Become(raft update, Leader(state, Some progress'), [])
 
     | state, msg -> Unhandled(state,msg)
